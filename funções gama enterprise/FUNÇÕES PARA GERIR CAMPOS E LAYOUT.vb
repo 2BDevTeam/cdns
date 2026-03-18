645c98a743bcb6b8be0a1437f4e49bfb4ae96035
dnelse2b <Denilson.Sibinde@2business-si.com>
2026-03-18 15:09:42 +0200

    Funções enterprise

---
+++ funções gama enterprise/FUNÇÕES PARA GERIR CAMPOS E LAYOUT.vb
@@ -0,0 +1,63 @@
+
+Function ocultarCampo(ByVal nomeConteudo, ByVal mpage)
+
+    Try
+        Dim obj As Object
+        obj = mpage.Master.FindControl("conteudo").FindControl(nomeConteudo)
+        obj.Visible = False
+
+    Catch ex As Exception
+
+    End Try
+
+End Function
+
+Function setValue(ByVal conteudo, ByVal value, ByVal mpage)
+
+    Dim obj As Object
+    obj = mpage.Master.FindControl("conteudo").FindControl(conteudo)
+    If obj IsNot Nothing Then
+        obj.value = value
+        obj.Refresh()
+        Return New With {.exists = True}
+    End If
+
+    Return New With {.exists = False}
+
+End Function
+
+Function alteraTituloForm(ByVal nomeTitulo, ByVal mpage)
+
+    CType(mpage, normalform).PropTitulo = nomeTitulo
+
+End Function
+
+Function setReadOnly(ByVal conteudo, ByVal readonlyPar, ByVal mpage)
+
+    Dim obj As Object
+    obj = mpage.Master.FindControl("conteudo").FindControl(conteudo)
+    obj.readonly = readonlyPar
+
+End Function
+
+Function alteraTituloCampo(ByVal nomeConteudo, ByVal novoTitulo, ByVal mpage)
+
+    Dim obj As Object
+    obj = mpage.Master.FindControl("conteudo").FindControl(nomeConteudo)
+    If Not obj Is Nothing Then
+
+        obj.TituloDoCampo = novoTitulo
+
+        ' Verificar se o objeto tem a propriedade/método RefreshTitulo
+        Dim objType As Type = obj.GetType()
+        Dim refreshMethod = objType.GetMethod("RefreshTitulo")
+
+        If refreshMethod IsNot Nothing Then
+            obj.RefreshTitulo()
+        Else
+            obj.RefreshTituloDoCampo()
+        End If
+
+    End If
+
+End Function
