bb95d0dda2601601bbfd38063624b3535473b4f9
miltonmponda-2b <milton.mponda@2business-si.com>
2026-07-22 14:23:57 +0200

    MDash tables.sql - TEMPLATE DASHBOARD STANDARD EXTENSION
    MDash tables
     - Adição de coluna descricaodireita na tabela MdashFilters
    TEMPLATE DASHBOARD STANDARD EXTENSION
     - Campo, nome de série é um campo de expressão Js multilinha

--- bibliotecas/Mdash 2.0/SQL/MDash tables.sql
+++ bibliotecas/Mdash 2.0/SQL/MDash tables.sql
@@ -130,7 +130,7 @@ CREATE TABLE MdashFilter(
     valordefeito TEXT DEFAULT '',
     ordem INT DEFAULT 0,
     escopo VARCHAR(20) NOT NULL DEFAULT 'global',  -- 'global' = todos os tabs; 'tab' = só nos separadores indicados
-    mdashtabstamp text NOT NULL DEFAULT ''   -- CSV de stamps MdashTab quando escopo = 'tab' (ex.: stamp1,stamp2)
+    mdashtabstamp text NOT NULL DEFAULT '',   -- CSV de stamps MdashTab quando escopo = 'tab' (ex.: stamp1,stamp2)
     descricaodireita VARCHAR(250) DEFAULT ''	
 );
 
